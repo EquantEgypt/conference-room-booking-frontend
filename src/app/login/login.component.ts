3a881170bdc8a76a8239287f5ce862d611ff82da
omarr78 <omernabil2001@gmail.com>
2025-09-04 09:58:16 +0300

    implement form validation with ReactiveForms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,55 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { NgClass, NgIf } from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, NgIf, NgClass],
+  imports: [FormsModule, NgIf, NgClass, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  username = '';
-  password = '';
-  invalidLogin = false;
+
+  loginForm: FormGroup;
+
+  // invalidLogin = false; 
   showPassword = false;
 
-  constructor() { }
 
-  onLogin() {
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [
+        Validators.required,
+        // Validators.minLength(3),
+        // Validators.maxLength(30),
+        Validators.pattern(/^[A-Za-z][A-Za-z0-9]*$/)  // must start with letter, then alphanumeric only
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(3), // temporarily just for testing security in backend
+        Validators.pattern(/^\S+$/) /* \S matches any non-whitespace character , + one or more  */
+      ]]
+    })
+  }
+
+  onLogin(): void {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      console.log(username, password);
+    }
+    else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+  get username(): AbstractControl | null {
+    return this.loginForm.get('username');
+  }
+
+  get password(): AbstractControl | null {
+    return this.loginForm.get('password');
   }
 
 }

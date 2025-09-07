6030308dc8ab2b69216947cd2fe09d9fc91583c3
omarr78 <omernabil2001@gmail.com>
2025-09-07 04:04:36 +0300

    add PageNotFound component with AuthGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from './guards/auth.guard';
 import { NoAuthGuard } from './guards/no-auth.guard';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent, canActivate : [NoAuthGuard] },
     { path: '', component: LoginComponent, canActivate : [NoAuthGuard] },
     { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+    { path: '**', component: PageNotFoundComponent,canActivate: [AuthGuard]}
 ];

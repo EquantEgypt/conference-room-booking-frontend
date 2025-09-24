2082433bca6e240cca6f025a9002f8e5163bc50e
Seifehab11-8 <semaziz2004@yahoo.com>
2025-09-24 21:11:07 +0300

    merge:

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,18 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './interceptor/token.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([tokenInterceptor])), provideAnimationsAsync()
+    provideHttpClient(withInterceptors([tokenInterceptor])),
+    provideAnimationsAsync(),
+    provideToastr({
+      timeOut: 10000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    })
   ]
 };
